781ef5bb45d18c74cf8592050a542eb4d81a1759
Yunis Huseynzade <yunisdev.04@gmail.com>
2020-08-29 12:45:13 +0400

    change

--- dbClient.py
+++ dbClient.py
@@ -7,17 +7,16 @@ class DBClient:
         self.file = _file
 
     def getDB(self):
-        with open(self.file, 'rt') as read_file:
+        with open("db.json", 'rt') as read_file:
             return json.load(read_file)
 
     def addUser(self, obj):
         with open(self.file, 'w') as source:
-            with open(self.file, 'r') as read_file:
-                current = json.load(read_file)
-                print(current)
-                current.users.append(obj)
-                json.dump(current, source)
-                print('Successfully added user.')
+            current = self.getDB()
+            print(current)
+            current.users.append(obj)
+            json.dump(current, source)
+            print('Successfully added user.')
 
     def getUser(self, _id):
         pass
